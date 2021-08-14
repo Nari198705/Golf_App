28374240e7c10e5803708215073aeb69c7bd428f
Nari <nhmtrm13@gmail.com>
2021-08-15 04:08:10 +0900

    router-view追加

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png" />
     <div>
       <p>
-        If Element is successfully added to this project, you'll see an
-        <code v-text="'<el-button>'"></code>
-        below
+        ユーザー名：<el-input
+          placeholder="Please input"
+          v-model="input"
+        ></el-input>
+        パスワード：<el-input
+          placeholder="Please input"
+          v-model="input"
+        ></el-input>
       </p>
-      <el-button>el-button</el-button>
+      <el-button>ログイン</el-button>
     </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/Round_Info">Round_Info</router-link> |
+    <router-view />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
